422bbe73d382e21ae625258f3ef15c9d4ab79f5d
Cam Spiers <camspiers@gmail.com>
2013-04-18 17:08:47 +1200

    UPDATE: Add config

--- code/DataObjects/StructuredContent.php
+++ code/DataObjects/StructuredContent.php
@@ -1,20 +1,35 @@
 <?php
 
+/**
+ * Class StructuredContent
+ */
 class StructuredContent extends DataObject
 {
-    static $extensions = array(
+    /**
+     * @var array
+     */
+    public static $extensions = array(
         "Hierarchy",
         "Versioned('Stage', 'Live')",
     );
+    /**
+     * @var array
+     */
     public static $db = array(
         'Title' => 'Varchar(255)',
         'SubTitle' => 'Varchar(255)',
         'Teaser' => 'Text',
         'ShortTeaser' => 'Text'
     );
+    /**
+     * @var array
+     */
     public static $has_one = array(
         'LeadImage' => 'StructuredContentImage'
     );
+    /**
+     * @var array
+     */
     public static $has_many = array(
         'HTMLs' => 'StructuredContentHTML',
         'Images' => 'StructuredContentImage',
