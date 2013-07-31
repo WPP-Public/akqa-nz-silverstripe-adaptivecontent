e7277f4c2e0e0f44efb5996acfc2d8b1bc798c6f
Cam Spiers <camspiers@gmail.com>
2013-07-31 16:54:49 +1200

    UPDATE: Tidy up hierarchy to work with ss 3

--- code/Extensions/AdaptiveContentHierarchy.php
+++ code/Extensions/AdaptiveContentHierarchy.php
@@ -13,11 +13,10 @@ class AdaptiveContentHierarchy extends DataExtension
      * @var array
      */
     private static $db = array(
-        'HierarchySortOrder'  => 'Varchar(255)',
         'HierarchyTitle'      => 'Varchar(255)',
         'HierarchyIdentifier' => 'Varchar(255)',
         'HierarchyDepth'      => 'Int',
-        'SortOrder'           => 'Int'
+        'Sort'                => 'Int'
     );
     /**
      * @var array
@@ -25,18 +24,6 @@ class AdaptiveContentHierarchy extends DataExtension
     private static $indexes = array(
         'HierarchyIdentifier' => true
     );
-    /**
-     * @param $fields
-     */
-    public function updateSummaryFields(&$fields)
-    {
-        $fields = array_merge(
-            $fields,
-            array(
-                'HierarchyTitle' => 'Title'
-            )
-        );
-    }
     /**
      * @param $class
      * @param $extension
@@ -55,46 +42,44 @@ class AdaptiveContentHierarchy extends DataExtension
         );
     }
     /**
-     * @param boolean $skipChildrenField
+     * @param $fields
      */
-    public function setSkipChildrenField($skipChildrenField)
+    public function updateSummaryFields(&$fields)
     {
-        $this->skipChildrenField = $skipChildrenField;
+        $fields = array_merge(
+            $fields,
+            array(
+                'HierarchyTitle' => 'Title'
+            )
+        );
     }
     /**
      *
      */
     public function onBeforeWrite()
     {
-        if ($this->owner->SortOrder === null && (array_key_exists(
-                    'ParentID',
-                    $this->owner->toMap()
-                ) || $this->owner->exists())
-        ) {
-            $this->owner->SortOrder = $this->getNextSortOrder();
-        }
         $parent = $this->owner;
-        $sorts = array(
-            $parent->getSortOrderChar()
-        );
+        
         $titles = array(
             $parent->Title
         );
+        
         $identifiers = array(
             $parent->Identifier
         );
+        
         while ($parent->ParentID) {
             $parent = $parent->Parent();
-            $sorts[] = $parent->getSortOrderChar();
             $titles[] = $parent->Title;
             $identifiers[] = $parent->Identifier;
         }
-        $this->owner->HierarchySortOrder = implode('', array_reverse($sorts));
+        
         $this->owner->HierarchyTitle = implode(' -> ', array_reverse($titles));
         $this->owner->HierarchyIdentifier = implode('/', array_reverse($identifiers));
         $this->owner->HierarchyDepth = count($identifiers) - 1;
 
         $children = $this->owner->Children();
+        
         if ($children instanceof IteratorAggregate) {
             foreach ($children as $child) {
                 $child->write();
@@ -106,55 +91,25 @@ class AdaptiveContentHierarchy extends DataExtension
      */
     public function updateCMSFields(FieldList $fields)
     {
-        $fields->removeByName('HierarchySortOrder');
         $fields->removeByName('HierarchyTitle');
         $fields->removeByName('Parent');
-        $fields->insertBefore(
-            $fields->dataFieldByName('SortOrder'),
-            'Identifier'
-        );
-    }
-    /**
-     * @return FieldSet
-     */
-    public function getLimitedCMSFields()
-    {
-        $this->setSkipChildrenField(true);
-        $fields = $this->owner->getCMSFields();
-        // TODO: This is a dependency
-        $fields->replaceField(
-            'HTML',
-            new HtmlEditorField(
-                'HTML',
-                'Html'
+        
+        $fields->addFieldToTab(
+            'Root.Children',
+            new GridField(
+                'Children',
+                'Children',
+                $this->owner->Children(),
+                $config = GridFieldConfig_RecordEditor::create()
             )
         );
 
-        return $fields;
-    }
-    /**
-     * @return string
-     */
-    public function getSortOrderChar()
-    {
-        $sortOrder = $this->owner->SortOrder + 33;
-        $num = $sortOrder / 126;
-        if ($num > 1) {
-            return str_repeat(chr(126), floor($num)) . chr($sortOrder % 126);
+        if ($this->owner->hasExtension('VersionedDataObject')) {
+            $config->addComponent(new GridFieldVersionedOrderableRows('Sort'));
+            $config->removeComponentsByType('GridFieldDetailForm');
+            $config->addComponent(new VersionedDataObjectDetailsForm());
         } else {
-            return chr($sortOrder);
-        }
-    }
-    /**
-     * @return int|mixed
-     */
-    protected function getNextSortOrder()
-    {
-        $siblings = $this->getSiblings();
-        if ($siblings instanceof DataList) {
-            return ($do = $siblings->last()) instanceof DataObject ? $do->SortOrder + 1 : 0;
-        } else {
-            return 0;
+            $config->addComponent(new GridFieldOrderableRows('Sort'));
         }
     }
     /**
